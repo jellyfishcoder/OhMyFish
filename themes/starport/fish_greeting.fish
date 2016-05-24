5d5c5d85bb96ba57e9c4afb80d2a55303df8fa89
Jellyfishcoder <jellyfishcoder@gmail.com>
2016-05-23 19:30:24 -0500

    Custom prompt and theme

--- themes/starport/fish_greeting.fish
+++ themes/starport/fish_greeting.fish
@@ -8,6 +8,7 @@
 #   set_color normal
 #
 
+## Custom Color Configurations ##
 function starport_error
 	set_color -o red
 end
@@ -16,5 +17,8 @@ function starport_normal
 	set_color normal
 end
 
+## Fish Opening Greeting
 function fish_greeting
+	set_color
+	printf
 end
--- themes/starport/fish_prompt.fish
+++ themes/starport/fish_prompt.fish
@@ -1,45 +1,81 @@
-function fish_prompt
-	## Customize colors here  ##
+function fish_prompt -d 'Starport, a simple, one-line not-too-fancy theme.'
+	# Save last command status before it gets away
+	set -l last_status $status
+
+	## Custom Icons ##
+	# Super user prompt symbol
+	set -xl suprompt "#"
+	# User prompt symbol
+	set -xl usprompt "\$"
+
+	## Custom Colors for Prompt ##
 	set -xl datecol blue
-	set -xl datetyp "--bold"
+	set -xl datetyp "-o"
 	set -xl exitcol red
-	set -xl exittyp ""
+	set -xl exittyp
 	set -xl usercol green
-	set -xl usertyp ""
+	set -xl usertyp
 	set -xl direcol magenta
-	set -xl diretyp "--bright"
-	set -xl prmtcol blue
-	set -xl prmttyp "--bright"
+	set -xl diretyp
+	set -xl supromptcolor red
+	set -xl suprompttype "-o"
+	set -xl uspromptcolor blue
+	set -xl usprompttype
+
+	## Check if Superuser
+	set -e superuser
+	[ (id -u $USER) -eq 0 ]
+		and set -l superuser 1
 
 	## Date ##
-	set_color $datecol
+	set_color $datecol $datetyp
 	printf (date +"%H:%M")
 	printf "> "
+	set_color normal -b normal
+
 	## Exit Status ##
-	set -l code $status
-	switch $status
+	switch $last_status
 		case '0'
 			# Nothing
-			set_color normal
+			set_color normal -b normal
 		case '*'
 			# Print the exit code since it didn't exit clean
 			set_color $exitcol $exittyp
 			printf "["
-			printf $status
+			printf "$last_status"
 			printf "]"
+			set_color normal -b normal
 	end
+
 	## User ##
 	set_color $usercol $usertyp
 	printf "$USER"
+	set_color normal -b normal
+
 	## Directory ##
 	set_color $direcol $diretyp
 	printf "@"
 	printf ":"
-	printf (pwd)
-	## Prompt ##
-	set_color $prmtcol $prmttyp
-	printf "\$"
-	## Space ##
+	printf (prompt_pwd)
 	set_color normal -b normal
+
+	## Space ##
 	printf " "
+	set_color normal -b normal
+
+	## Prompt ##
+	if eval $superuser
+		# Print superuser prompt
+		set_color $supromptcolor $suprompttype
+		printf "$suprompt"
+	else
+		# Print standard user prompt
+		set_color $uspromptcolor $usprompttype
+		printf "$usprompt"
+	end
+	set_color normal -b normal
+
+	## Space ##
+	printf " "
+	set_color normal -b normal
 end
